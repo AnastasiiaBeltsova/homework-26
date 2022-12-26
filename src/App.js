20327466afec0e1525ac9680af129df365826133
AnastasiiaBeltsova <anastasiya.b145@gmail.com>
2022-12-26 22:13:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const user = {
+        firstName: "Anastasiia",
+        lastName: "Beltsova",
+        technologies: "HTML, CSS, JavaScript"
+    }
+
+
+    return (
+        <div className="App">
+          <p>My name: {user.firstName}</p>
+          <p>My surname: {user.lastName}</p>
+          <p>Learned Technologies: {user.technologies}</p>    
+        </div>
+    )
 }
 
 export default App;
